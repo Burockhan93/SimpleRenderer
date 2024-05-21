b8a464506d5a9243bd792fad6152673116827d23
Burakhan Dogan <burakhandogan93@gmail.com>
2024-05-21 21:13:30 +0200

    WireMesh Image Creation

--- SimpleRenderer/SimpleRenderer.cpp
+++ SimpleRenderer/SimpleRenderer.cpp
@@ -1,16 +1,24 @@
 #include "tgaimage.h"
 
+#include "Model.h"
+
+#define FILEPATH "obj\african_head.obj"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+Model* model = NULL;
+const int width = 8000;
+const int height = 8000;
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
     bool steep = false;
+    //for steep image we want to transpose the image
     if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
         std::swap(x0, y0);
         std::swap(x1, y1);
         steep = true;
     }
+    // if for some reason is the coordinate from right to left, make it from left to right
     if (x0 > x1) {
         std::swap(x0, x1);
         std::swap(y0, y1);
@@ -36,13 +44,26 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 }
 
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, red);
-	image.set(57, 46, white);
-	image.set(47, 36, white);
-	image.set(57, 36, white);
-	image.set(47, 46, white);
+
+    model = new Model("obj/kelly_belly.obj");
+
+    TGAImage image(width, height, TGAImage::RGB);
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+            int x0 = (v0.x + 1.) * width / 2.;
+            int y0 = (v0.y + 1.) * height / 2.;
+            int x1 = (v1.x + 1.) * width / 2.;
+            int y1 = (v1.y + 1.) * height / 2.;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
+
+    delete model;
+
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output2.tga");
+	image.write_tga_file("kelly_belly.tga");
 	return 0;
 }
