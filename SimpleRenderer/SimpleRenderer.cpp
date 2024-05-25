94624ee1ee7f9887bc69ec5adc5314f6c985a2e3
Burakhan Dogan <burakhandogan93@gmail.com>
2024-05-25 21:09:23 +0200

    Bugs removed

--- SimpleRenderer/SimpleRenderer.cpp
+++ SimpleRenderer/SimpleRenderer.cpp
@@ -5,6 +5,7 @@
 #define FILEPATH "obj/african_head.obj"
 #define FILEPATHWIRE "african_wireframe.tga"
 #define FILEPATHSHADE "african_shaded.tga"
+#define FILEPATHSHADELIGHT "african_shaded_light.tga"
 #define FILEPATHKELLY "obj/kelly_belly.obj"
 #define FILEPATHKELLYWIRE "kelly_belly_wireframe.tga"
 #define FILEPATHKELLYSHADER "kelly_belly_shaded.tga"
@@ -81,6 +82,27 @@ Vec3f barycentric(Vec2i* pts, Vec2i P) {
     if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1);
     return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
 }
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+    if (t0.y == t1.y && t0.y == t2.y) return; //This doesnt build a triangle, discard
+    //Order them from top to bottom.P(0,0) is bottom left.
+    if (t0.y > t1.y) std::swap(t0, t1);
+    if (t0.y > t2.y) std::swap(t0, t2);
+    if (t1.y > t2.y) std::swap(t1, t2);
+    //Total height is important for bounding box.
+    int total_height = t2.y - t0.y;
+    for (int i = 0; i < total_height; i++) {
+        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+        float alpha = (float)i / total_height;
+        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+        if (A.x > B.x) std::swap(A, B);
+        for (int j = A.x; j <= B.x; j++) {
+            image.set(j, t0.y + i, color); // attention, due to int casts t0.y+i != A.y
+        }
+    }
+}
 void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
 
     //Find the bounding box, 0,0 is bottom left
@@ -94,6 +116,7 @@ void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
         bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
         bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
     }
+    //Find the raster point
     Vec2i P{};
     for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
         for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
@@ -125,19 +148,25 @@ int main(int argc, char** argv) {
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file(FILEPATHWIRE);
     image.clear();
-    
     //Shading
+    Vec3f light_dir(0, 0, -1); // define light_dir
     for (int i = 0; i < model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
         Vec2i screen_coords[3];
+        Vec3f world_coords[3];
         for (int j = 0; j < 3; j++) {
-            Vec3f world_coords = model->vert(face[j]);
-            screen_coords[j] = Vec2i((world_coords.x + 1.) * width / 2., (world_coords.y + 1.) * height / 2.);
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+            world_coords[j] = v;
         }
-        triangle(screen_coords, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+        Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+        n.normalize();
+        float intensity = n * light_dir;
+        if(intensity>0)
+        triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity* 255, intensity * 255, intensity * 255, 255));
     }
     image.flip_vertically();
-    image.write_tga_file(FILEPATHSHADE);
+    image.write_tga_file(FILEPATHSHADELIGHT);
     delete model;
 
 	
